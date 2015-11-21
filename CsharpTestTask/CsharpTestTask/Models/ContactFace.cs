c4d0e6a791790cea760f143b561a97effe93fc4f
markusFrost <andreywystawkin@yandex.ru>
2015-11-21 15:05:19 +0300

    create ContactFace Model

---
+++ CsharpTestTask/CsharpTestTask/Models/ContactFace.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CsharpTestTask.Models
+{
+    public class ContactFace
+    {
+        public int Id {get; set;}
+
+        public string Surname { get; set; }
+
+        public string Name { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public int WorkPhone { get; set; }
+
+        public int MobilePhone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
