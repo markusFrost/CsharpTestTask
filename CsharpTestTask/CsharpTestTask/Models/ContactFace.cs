8d9ae813777213ab07bc95e897dd0d781bc07df1
markusFrost <andreywystawkin@yandex.ru>
2015-11-21 15:11:06 +0300

    add validation to ContactFace Model

--- CsharpTestTask/CsharpTestTask/Models/ContactFace.cs
+++ CsharpTestTask/CsharpTestTask/Models/ContactFace.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,17 +9,24 @@ namespace CsharpTestTask.Models
     public class ContactFace
     {
         public int Id {get; set;}
-
+        
+        [Required (ErrorMessage="Please enter surname", AllowEmptyStrings=false) ]
         public string Surname { get; set; }
 
+        [Required(ErrorMessage = "Please enter name", AllowEmptyStrings = false)]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Please enter patronymic", AllowEmptyStrings = false)]
         public string Patronymic { get; set; }
 
-        public int WorkPhone { get; set; }
+        [Required(ErrorMessage = "Please enter work phone", AllowEmptyStrings = false)]
+        public string WorkPhone { get; set; }
 
-        public int MobilePhone { get; set; }
+        [Required(ErrorMessage = "Please enter mobile phone", AllowEmptyStrings = false)]
+        public string MobilePhone { get; set; }
 
+        [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,3})$",
+                    ErrorMessage = "Please provide valid email id")]
         public string Email { get; set; }
     }
 }
