c6f3a5756d8b9b60fe72968bfe16c8020f4504a3
markusFrost <andreywystawkin@yandex.ru>
2015-11-21 15:16:45 +0300

    add view to register ClientFace Model

---
+++ CsharpTestTask/CsharpTestTask/Controllers/ContactFaceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CsharpTestTask.Controllers
+{
+    public class ContactFaceController : Controller
+    {
+        //
+        // GET: /ContactFace/
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
--- CsharpTestTask/CsharpTestTask/Models/ContactFace.cs
+++ CsharpTestTask/CsharpTestTask/Models/ContactFace.cs
@@ -27,6 +27,7 @@ namespace CsharpTestTask.Models
 
         [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,3})$",
                     ErrorMessage = "Please provide valid email id")]
+        [Required(ErrorMessage = "Please enter email", AllowEmptyStrings = false)]
         public string Email { get; set; }
     }
 }
