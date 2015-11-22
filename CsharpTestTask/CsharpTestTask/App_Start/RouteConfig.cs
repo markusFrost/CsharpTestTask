55e029fbfcaefd4108390953273d2ef32b96cd05
markusFrost <andreywystawkin@yandex.ru>
2015-11-22 16:52:43 +0300

    add bootstrap to getUser page

--- CsharpTestTask/CsharpTestTask/App_Start/RouteConfig.cs
+++ CsharpTestTask/CsharpTestTask/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CsharpTestTask
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "User", action = "getUsers", id = UrlParameter.Optional }
             );
         }
     }
